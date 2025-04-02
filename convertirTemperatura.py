cd28b9327cb65297286d2e649117894b9e8e7a3a
EmperorKnight <jlpicado@uamv.edu.ni>
2025-04-02 14:46:19 -0600

    Modifique y agregue

--- convertirTemperatura.py
+++ convertirTemperatura.py
@@ -1,5 +1,3 @@
 grado_f = float(input("Grados fahrenheit: ")) #float = real
 grado_c = ((grado_f - 32)*5)/9
-#grado_c *= 5
-#grado_c /= 9
 print("C",grado_c)
--- tiempo_vuelo.py
+++ tiempo_vuelo.py
@@ -1,8 +1,8 @@
-pTramo = int(input("Introduzca el tiempo del primer tramo del vuelo:"))
-pEscala = int(input("Introduzca el tiempo de la primera escala:"))
-sTramo = int(input("Introduzca el tiempo del segundo tramo del vuelo:"))
-sEscala = int(input("Introduzca el tiempo de la segunda escala:"))
-tTramo = int(input("Introduzca el tiempo del tercer tramo del vuelo:"))
+pTramo = int(input("Introduzca el tiempo del primer tramo del vuelo: "))
+pEscala = int(input("Introduzca el tiempo de la primera escala: "))
+sTramo = int(input("Introduzca el tiempo del segundo tramo del vuelo: "))
+sEscala = int(input("Introduzca el tiempo de la segunda escala: "))
+tTramo = int(input("Introduzca el tiempo del tercer tramo del vuelo: "))
 	
 tiempo_viaje = ((((pTramo+pEscala)+sTramo)+sEscala)+tTramo)
 	
