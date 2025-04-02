f92ba37988cb2fcd18a1321a0aac8c8f78e1fdce
EmperorKnight <jlpicado@uamv.edu.ni>
2025-04-02 14:22:01 -0600

    Agregado

---
+++ tiempo_vuelo.py
@@ -0,0 +1,9 @@
+pTramo = int(input("Introduzca el tiempo del primer tramo del vuelo:"))
+pEscala = int(input("Introduzca el tiempo de la primera escala:"))
+sTramo = int(input("Introduzca el tiempo del segundo tramo del vuelo:"))
+sEscala = int(input("Introduzca el tiempo de la segunda escala:"))
+tTramo = int(input("Introduzca el tiempo del tercer tramo del vuelo:"))
+	
+tiempo_viaje = ((((pTramo+pEscala)+sTramo)+sEscala)+tTramo)
+	
+print("El tiempo total del vuelo es: ", tiempo_viaje)
